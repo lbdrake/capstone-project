c807d29b4d5ad97379c6aceb5627f5d32d6c1471
Leah Drake <drake.leah@gmail.com>
2015-10-13 10:44:30 -0700

    Add Session controller and sign in View

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(session_params[:username], session_params[:password])
+    if @user
+      signin(@user)
+      redirect_to projects_url
+    else
+      flash[:errors] = ["Incorrect Username/Password combo, please try again"]
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    signout
+    redirect_to new_session_url
+  end
+
+  private
+  def session_params
+    params.require(:users).permit(:username, :password)
+  end
 end
