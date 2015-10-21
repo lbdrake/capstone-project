c273e1d8cb8ec5a99a862799f12a1cf5b5431c22
Leah Drake <drake.leah@gmail.com>
2015-10-21 11:20:12 -0700

    Add Trash icon next to To Do List and add styling to appear on hover

--- app/assets/javascripts/components/tasks.js.jsx
+++ app/assets/javascripts/components/tasks.js.jsx
@@ -3,10 +3,16 @@ window.Tasks = React.createClass({
     console.log("Clicked 'Add New Task' button - will load form");
   },
 
+  handleDeleteToDoListClick: function (e) {
+    setTimeout(function () {if (window.confirm("Are you sure you want to delete this to do list?")) {
+      console.log("Clicked 'Delete to do list button'")
+    }}, 200)
+  },
+
   render: function () {
     return (
       <ul className="ulForToDoLists">
-        <li key={this.props.todolisttitle}>{this.props.todolisttitle}</li>
+        <li key={this.props.todolisttitle} className="todolist-title">{this.props.todolisttitle} <span onClick={this.handleDeleteToDoListClick} className="glyphicon glyphicon-trash delete-todolist-icon"></span></li>
         <li>
           <ul>
             {
