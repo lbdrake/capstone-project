bb715c0515341c74c4cfce4fc35bff56915a0c20
Leah Drake <drake.leah@gmail.com>
2015-10-25 16:02:55 -0700

    Update navbar

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -47,46 +47,67 @@ window.NavBar = React.createClass({
 
   render: function (){
     return (
-      <nav className="nav navbar-default nav-group">
-        <div className="container-fluid">
+      // <nav className="nav navbar-default nav-group">
+      //   <div className="container-fluid">
+      //
+      //     <div className="navbar-header">
+      //       <button type="button" className="navbar-toggle collapsed"
+      //               data-toggle="collapse"
+      //               data-target="#navbar-collapse-1"
+      //               aria-expanded="false" >
+      //         <span className="sr-only">Toggle navigation</span>
+      //         <span className="icon-bar"></span>
+      //         <span className="icon-bar"></span>
+      //         <span className="icon-bar"></span>
+      //       </button>
+      //       <a className="navbar navbar-brand" href="#" >
+      //         <img src="/assets/TaskMaster-logo.png" alt="TaskMaster Logo" />
+      //       </a>
+      //
+      //     </div>
+      //     <div className="collapse navbar-collapse" id="navbar-collapse-1">
+      //       <ul className="nav navbar-nav nav-center nav-bling-container">
+      //         <li className="nav-bling"><a href="#">Projects</a></li>
+      //         <li className="nav-bling"><a href="#">Calendar</a></li>
+      //         <li className="nav-bling"><a onClick={this.goToMyTasksPage}>Me <span className="label label-danger label-as-badge">{this.state.my_tasks_notifications}</span></a></li>
+      //       </ul>
+      //
+      //       <ul className="nav nav-stacked navbar-right">
+      //         <li className="navbar-text nav-username">{"Signed in as " + window.CURRENT_USERNAME}</li>
+      //         <li><a href="#logout" className="nav-logout" onClick={this.logoutUser}>Logout</a></li>
+      //       </ul>
+      //     </div>
+      //   </div>
+      // </nav>
 
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed"
-                    data-toggle="collapse"
-                    data-target="#navbar-collapse-1"
-                    aria-expanded="false" >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar navbar-brand" href="#" >
-              <img src="/assets/TaskMaster-logo.png" alt="TaskMaster Logo" />
-            </a>
-          </div>
-
-          <div className="collapse navbar-collapse" id="navbar-collapse-1">
-            <ul className="nav navbar-nav nav-center nav-bling-container">
-              <li className="nav-bling"><a href="#">Projects</a></li>
-              <li className="nav-bling"><a href="#">Calendar</a></li>
-              <li className="nav-bling"><a onClick={this.goToMyTasksPage}>Me <span className="label label-danger label-as-badge">{this.state.my_tasks_notifications}</span></a></li>
-            </ul>
-
-            <ul className="nav nav-stacked navbar-right">
-                <ul className="nav navbar-nav navbar-links navbar-double-links">
-                  <li className="navbar-text nav-username">{"Signed in as " + window.CURRENT_USERNAME}</li>
-                  <li><a href="#logout" className="nav-logout" onClick={this.logoutUser}>Logout</a></li>
-                </ul>
-              <form className="navbar-form" role="search">
-                  <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Search projects for..." />
-                  </div>
-                  <button type="submit" className="btn btn-default">Go!</button>
-              </form>
-            </ul>
-          </div>
+      <nav className="navbar navbar-default">
+  <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed"
+                  data-toggle="collapse"
+                  data-target="#navbar-collapse-1"
+                  aria-expanded="false" >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+            <img className="navbar-brand" src="/assets/TaskMaster-logo.png" alt="TaskMaster Logo" />
         </div>
-      </nav>
+
+    <div className="collapse navbar-collapse" id="navbar-collapse-1">
+      <ul className="nav navbar-nav nav-bling-container">
+        <li className="nav-bling"><a href="#">Projects</a></li>
+        <li className="nav-bling"><a onClick={this.goToMyTasksPage}>Me <span className="label label-danger label-as-badge">{this.state.my_tasks_notifications}</span></a></li>
+      </ul>
+
+      <ul className="nav navbar-nav navbar-right">
+        <li className="navbar-text nav-username">{"Signed in as " + window.CURRENT_USERNAME}</li>
+        <li><a href="#logout" className="nav-logout" onClick={this.logoutUser}>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
     );
   }
 });
